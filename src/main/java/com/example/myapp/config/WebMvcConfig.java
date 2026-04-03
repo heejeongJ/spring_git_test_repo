2b0cc4797677feabad666aab46cdfdf08d6ffe00
hee_!J <heeej920@naver.com>
2026-04-03 15:07:05 +0900

    hotfix: WebMvcConfig 빈설정 추가

--- src/main/java/com/example/myapp/config/WebMvcConfig.java
+++ src/main/java/com/example/myapp/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
 	@Bean(value = "localeResolver")
